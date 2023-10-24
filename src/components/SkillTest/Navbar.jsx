ef74e28960efaea8d5134864c3bbc9f05c08e629
unknown <Rajan Kumar>
2023-10-25 00:02:00 +0530

    navbar completed with responsiveness

---
+++ src/components/SkillTest/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../../styles/SkillTest/Navbar.css';
+import cuvette from '../../assets/images/cuvette_logo.png';
+import user from '../../assets/images/user_logo.png';
+
+function Navbar() {
+  return (
+    <div>
+        <div className='navbar-container'>
+          <div className='container'>
+             <div className='logo'>
+             <img src={cuvette} alt="cuvette_logo" />
+             </div>
+             <div className='top-bar'>
+               <div>
+               <img src={user} alt="user_logo" />
+                <p>Siddharth Jain</p>
+               </div>
+             </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
