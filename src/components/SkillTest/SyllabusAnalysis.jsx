7a2d1d315e52713d949231cb77c0f02918cb0583
unknown <Rajan Kumar>
2023-10-26 17:59:53 +0530

    minimization of syllabus component code

--- src/components/SkillTest/SyllabusAnalysis.jsx
+++ src/components/SkillTest/SyllabusAnalysis.jsx
@@ -3,28 +3,10 @@ import "../../styles/SkillTest/SyllabusAnalysis.css";
 
 function SyllabusAnalysis() {
   const syllabusItems = [
-    {
-      label: "HTML Tools, Forms, History",
-      color: "rgba(67, 138, 246, 1)",
-      percentage: 80,
-    },
-    {
-      label: "Tags & References in HTML",
-      color: "rgba(255, 145, 66, 1)",
-      percentage: 60,
-    },
-    {
-      label: "Tables & CSS Basics",
-      color: "rgba(251, 94, 94, 1)",
-      percentage: 24,
-      marginRight: "112px", // Add marginRight here
-    },
-    {
-      label: "Tables & CSS Basics",
-      color: "rgba(46, 201, 113, 1)",
-      percentage: 96,
-      marginRight: "112px", // Add marginRight here
-    },
+    { label: "HTML Tools, Forms, History", color: "rgba(67, 138, 246, 1)", percentage: 80 },
+    { label: "Tags & References in HTML", color: "rgba(255, 145, 66, 1)", percentage: 60 },
+    { label: "Tables & CSS Basics", color: "rgba(251, 94, 94, 1)", percentage: 24, marginRight: "112px" },
+    { label: "Tables & CSS Basics", color: "rgba(46, 201, 113, 1)", percentage: 96, marginRight: "112px" },
   ];
 
   return (
@@ -35,32 +17,16 @@ function SyllabusAnalysis() {
         {syllabusItems.map((item, index) => (
           <div className="horizontal-bar-graph" key={index}>
             <div className="about-input">
-              <label style={{ marginRight: item.marginRight }}>
-                {item.label}
-              </label>
+              <label style={{ marginRight: item.marginRight }}>{item.label}</label>
             </div>
             <div className="syllabus-container">
               <div className="input-graph">
                 <div className="rounded-graph">
-                  <div
-                    className="progress"
-                    style={{
-                      width: `${item.percentage}%`, // Set the width dynamically
-                      background: item.color,
-                    }}
-                  ></div>
+                  <div className="progress" style={{ width: `${item.percentage}%`, background: item.color }}></div>
                 </div>
               </div>
               <div className="percentage">
-                <span
-                  style={{
-                    color: item.color,
-                    fontSize: "17px",
-                    fontWeight: "700",
-                  }}
-                >
-                  {item.percentage}%
-                </span>
+                <span style={{ color: item.color, fontSize: "17px", fontWeight: "700" }}>{item.percentage}%</span>
               </div>
             </div>
           </div>
@@ -71,4 +37,3 @@ function SyllabusAnalysis() {
 }
 
 export default SyllabusAnalysis;
-
