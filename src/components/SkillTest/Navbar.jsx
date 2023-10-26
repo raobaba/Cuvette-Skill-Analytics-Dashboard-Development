173287739a57ee2d1f4230f43890fb275c50b5fd
unknown <Rajan Kumar>
2023-10-26 16:50:03 +0530

    udpated the className and written meaningful comment in the Navbar component

--- src/components/SkillTest/Navbar.jsx
+++ src/components/SkillTest/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import "../../styles/SkillTest/Navbar.css";
+import "../../styles/SkillTest/Navbar.css"; // Import the CSS file
 import cuvette from "../../assets/images/cuvette_logo.png";
 import user from "../../assets/images/user_logo.png";
 
 function Navbar() {
   return (
     <div>
-      <div className="navbar-container">
-        <div className="container">
-          <div className="logo">
+      <div className="custom-navbar-container"> {/* Renamed the container for the entire navbar */}
+        <div className="custom-container"> {/* Renamed the inner container */}
+          <div className="custom-logo"> {/* Renamed the logo container */}
             <img src={cuvette} alt="cuvette_logo" />
           </div>
-          <div className="top-bar">
+          <div className="custom-top-bar"> {/* Renamed the top bar container */}
             <div>
               <img src={user} alt="user_logo" />
               <span>Siddharth Jain</span>
